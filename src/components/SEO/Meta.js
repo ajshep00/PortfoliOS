fd7920f754f76aecd58fbbf0c8c93761e555f67b
ajshep00 <ajshep00@proton.me>
2024-09-01 14:19:46 -0700

    Removed pdfworker.js and removed comment from Meta.js

--- src/components/SEO/Meta.js
+++ src/components/SEO/Meta.js
@@ -4,7 +4,6 @@ import Head from 'next/head';
 export default function Meta() {
     return (
         <Head>
-           /* Primary Meta Tags */
             <title>PortfoliOS | Alex Shepherd Portfolio</title>
             <meta charSet="utf-8" />
             <meta name="title" content="Alex Shepherd - Desktop Portfolio" />
