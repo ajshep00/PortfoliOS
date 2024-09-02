9f230c68e3dc695445ff0d6a257f8e4da99ac7d5
ajshep00 <ajshep00@proton.me>
2024-09-02 15:04:44 -0700

    Updated script location

--- src/components/SEO/Meta.js
+++ src/components/SEO/Meta.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import Head from 'next/head';
-import Script from 'next/script'
 
 export default function Meta() {
-    const faviconGif = '/files/artwork/favicon_frames/os_favicon.gif'
     return (
         <>
             <title>PortfoliOS | Alex Shepherd Portfolio</title>
             <meta charSet="utf-8" />
             <meta name="title" content="Alex Shepherd - Desktop Portfolio" />
-            {/* <link rel="icon" href={faviconGif} type="image/gif" ></link> */}
-            <link id="dynamic-favicon" rel="icon" href="/files/artwork/favicon_frames/frame_00.png" />
-            <Script src="/files/favicon.js" />
-            {/* <link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32" /> */}
+            
         </>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,17 @@
-// pages/_app.tsx
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Head from 'next/head';
+import Meta from '@/components/SEO/Meta'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>PortfoliOS | Alex Shepherd Portfolio</title>
-        <meta charSet="utf-8" />
-        <meta name="title" content="Alex Shepherd - Desktop Portfolio" />
+        <Meta />
         <link id="dynamic-favicon" rel="icon" href="/files/artwork/favicon_frames/frame_00.png" />
       </Head>
-      <Script src="/files/favicon.js" />
+      <Script src="/files/scripts/favicon.js" />
       <Component {...pageProps} />
     </>
   );
