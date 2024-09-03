a36dfdc6ef8a89075f831b23722ba315c7ae0af6
ajshep00 <ajshep00@proton.me>
2024-09-03 03:49:56 -0700

    Moved elements from Meta into Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import Head from 'next/head';
-import Meta from '@/components/SEO/Meta'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <Meta />
+        <title>PortfoliOS | Alex Shepherd Portfolio</title>
+        <meta charSet="utf-8" />
+        <meta name="title" content="Alex Shepherd - Desktop Portfolio" />
         <link id="dynamic-favicon" rel="icon" href="/files/artwork/favicon_frames/frame_00.png" />
       </Head>
       <Script src="/files/scripts/favicon.js" />
