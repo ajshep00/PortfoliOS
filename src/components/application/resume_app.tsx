767d1235534c0ed7f55209e5c4621f39ca9d336e
ajshep00 <ajshep00@proton.me>
2024-09-09 08:05:03 -0700

    Updated Resume

--- src/components/application/resume_app.tsx
+++ src/components/application/resume_app.tsx
@@ -1,7 +1,7 @@
   
   const ResumeApp: React.FC = () => {
 
-    const pdfUrl = './files/pdf/resume/honors-noFC_engineeringresumes.pdf';
+    const pdfUrl = './files/pdf/resume/Alex Shepherd Resume.pdf';
 
     return (
             <div className='relative h-[500px] max-h-2xl rounded-lg overflow-hidden'>
